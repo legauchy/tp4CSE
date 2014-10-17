ed0da51374ca2af2054ffa12ff9b3fb11081b745
gauchya <antho2gauchy@gmail.com>
2014-10-17 14:51:59 +0200

    Q4 fais, toute les questions opti

--- Q3/lectred.c
+++ Q3/lectred.c
@@ -4,22 +4,25 @@ struct lectred {
 	sem_t mutex_nb_lecteur;
 	sem_t mutex_prio_lect;
 	int nb_lecteur;
-	sem_t henry;
+	sem_t sem_principal;
 };
 
 struct lectred* lectred_init() {
 	struct lectred * lectred = malloc(sizeof(struct lectred));
+	
 	sem_init(&lectred->mutex_nb_lecteur, 0, 1);
-	sem_init(&lectred->mutex_prio_lect, 0, 1);
-	sem_init(&lectred->henry, 0, 1);
 	lectred->nb_lecteur = 0;
+	sem_init(&lectred->mutex_prio_lect, 0, 1);
+	
+	sem_init(&lectred->sem_principal, 0, 1);
+	
 	return lectred;
 }
 
 void begin_read(struct lectred* lectred) {
 	sem_wait(&lectred->mutex_nb_lecteur);
 	if(lectred->nb_lecteur == 0) {
-		sem_wait(&lectred->henry);
+		sem_wait(&lectred->sem_principal);
 	}
 	lectred->nb_lecteur++;
 	sem_post(&lectred->mutex_nb_lecteur);
@@ -29,23 +32,24 @@ void end_read(struct lectred* lectred) {
 	sem_wait(&lectred->mutex_nb_lecteur);
 	lectred->nb_lecteur--;
 	if(lectred->nb_lecteur == 0) {
-		sem_post(&lectred->henry);
+		sem_post(&lectred->sem_principal);
 	}
 	sem_post(&lectred->mutex_nb_lecteur);
 }
 
 void begin_write(struct lectred* lectred) {
 	sem_wait(&lectred->mutex_prio_lect);
-	sem_wait(&lectred->henry);
+	sem_wait(&lectred->sem_principal);
 }
 
 void end_write(struct lectred* lectred) {
-	sem_post(&lectred->henry);
+	sem_post(&lectred->sem_principal);
 	sem_post(&lectred->mutex_prio_lect);
 }
 
 void lectred_destroy(struct lectred* lectred) {
-	sem_destroy(&lectred->henry);
+	sem_destroy(&lectred->sem_principal);
 	sem_destroy(&lectred->mutex_nb_lecteur);
+	sem_destroy(&lectred->mutex_prio_lect);
 	free(lectred);
 }
--- Q4/lectred.c
+++ Q4/lectred.c
@@ -1,49 +1,76 @@
 #include "lectred.h"
 
 struct lectred {
-	sem_t mutex_nb_lecteur;
-	sem_t mutex_prio_writ;
-	int nb_lecteur;
-	sem_t henry;
+	sem_t mutex_nb_lecteur_en_cour;
+	int nb_lecteur_en_cour;
+	
+	sem_t mutex_nb_writ_en_attente;
+	int nb_writ_en_attente;
+	sem_t sem_prio_writ;
+	
+	sem_t sem_principal;
 };
 
 struct lectred* lectred_init() {
 	struct lectred * lectred = malloc(sizeof(struct lectred));
-	sem_init(&lectred->mutex_nb_lecteur, 0, 1);
-	sem_init(&lectred->mutex_prio_lect, 0, 1);
-	sem_init(&lectred->henry, 0, 1);
-	lectred->nb_lecteur = 0;
+	sem_init(&lectred->mutex_nb_lecteur_en_cour, 0, 1);
+	lectred->nb_lecteur_en_cour = 0;
+	
+	sem_init(&lectred->sem_prio_writ, 0, 1);
+	sem_init(&lectred->mutex_nb_writ_en_attente, 0, 1);
+	lectred->nb_writ_en_attente = 0;
+	
+	sem_init(&lectred->sem_principal, 0, 1);
 	return lectred;
 }
 
 void begin_read(struct lectred* lectred) {
-	sem_wait(&lectred->mutex_nb_lecteur);
-	if(lectred->nb_lecteur == 0) {
-		sem_wait(&lectred->henry);
+	sem_wait(&lectred->sem_prio_writ);
+	sem_post(&lectred->sem_prio_writ);
+	sem_wait(&lectred->mutex_nb_lecteur_en_cour);
+	if(lectred->nb_lecteur_en_cour == 0) {
+		sem_wait(&lectred->sem_principal);
 	}
-	lectred->nb_lecteur++;
-	sem_post(&lectred->mutex_nb_lecteur);
+	lectred->nb_lecteur_en_cour++;
+	sem_post(&lectred->mutex_nb_lecteur_en_cour);
 }
 
 void end_read(struct lectred* lectred) {
-	sem_wait(&lectred->mutex_nb_lecteur);
-	lectred->nb_lecteur--;
-	if(lectred->nb_lecteur == 0) {
-		sem_post(&lectred->henry);
+	sem_wait(&lectred->mutex_nb_lecteur_en_cour);
+	lectred->nb_lecteur_en_cour--;
+	if(lectred->nb_lecteur_en_cour == 0) {
+		sem_post(&lectred->sem_principal);
 	}
-	sem_post(&lectred->mutex_nb_lecteur);
+	sem_post(&lectred->mutex_nb_lecteur_en_cour);
 }
 
 void begin_write(struct lectred* lectred) {
-	sem_wait(&lectred->henry);
+	sem_wait(&lectred->mutex_nb_writ_en_attente);
+	if(lectred->nb_writ_en_attente == 0) {
+		sem_trywait(&lectred->sem_prio_writ);
+	}
+	lectred->nb_writ_en_attente++;
+	sem_post(&lectred->mutex_nb_writ_en_attente);
+	sem_wait(&lectred->sem_principal);
 }
 
 void end_write(struct lectred* lectred) {
-	sem_post(&lectred->henry);
+	sem_wait(&lectred->mutex_nb_writ_en_attente);
+	lectred->nb_writ_en_attente--;
+	if(lectred->nb_writ_en_attente == 0) {
+		sem_post(&lectred->sem_prio_writ);
+	}
+	sem_post(&lectred->mutex_nb_writ_en_attente);
+	sem_post(&lectred->sem_principal);
 }
 
 void lectred_destroy(struct lectred* lectred) {
-	sem_destroy(&lectred->henry);
-	sem_destroy(&lectred->mutex_nb_lecteur);
+	sem_destroy(&lectred->mutex_nb_lecteur_en_cour);
+	
+	sem_destroy(&lectred->sem_prio_writ);
+	sem_destroy(&lectred->mutex_nb_writ_en_attente);
+	
+	sem_destroy(&lectred->sem_principal);
+
 	free(lectred);
 }
