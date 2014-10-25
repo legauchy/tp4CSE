ae5230448c1b1313bf85a7fa93e34ba6fcc82889
gauchya <antho2gauchy@gmail.com>
2014-10-25 14:11:07 +0200

    Fifo ok, condition des if et surtout des while a verif

--- Q5_cour/lectred.c
+++ Q5_cour/lectred.c
@@ -4,21 +4,19 @@ struct lectred {
 	pthread_mutex_t mutex;
 	int nb_l;
 	int nb_w;
-	struct liste * first;
-	struct liste * last;
+	struct list * first;
+	struct list * last;
 };
 
-struct liste {
-	pthread_cond_t * maCond;
+struct list {
+	pthread_cond_t * ma_cond;
 	int est_lect;
-	struct liste * suivant;
+	struct list * suivant;
 };
 
 struct lectred* lectred_init() {
 	struct lectred * lectred = malloc(sizeof(struct lectred));
 	pthread_mutex_init(&lectred->mutex, NULL);
-	pthread_cond_init(&lectred->lect, NULL);
-	pthread_cond_init(&lectred->red, NULL);
 	lectred->nb_w = 0;
 	lectred->nb_l = 0;
 	lectred->first = NULL;
@@ -26,24 +24,54 @@ struct lectred* lectred_init() {
 	return lectred;
 }
 
-int add_thread_list(struct lectred* lectred, int bl) {
+pthread_cond_t * add_thread_list(struct lectred* lectred, int bl) {
+	struct list * l = malloc(sizeof(struct list));
+	l->ma_cond = malloc(sizeof(pthread_cond_t));
+	pthread_cond_init(l->ma_cond, NULL);
+	l->est_lect = bl;
+	l->suivant = NULL;
+	
 	if(lectred->first == NULL) { //Liste vide
-		struct list * l = malloc(sizeof(struct list));
-		l->est_lect = bl;
-		l->suivant = NULL;
+		lectred->first = l;
 		lectred->last = l;
 	} else {
-		struct list * l = malloc(sizeof(struct list));
-		l->i = lectred->;
+		lectred->last->suivant = l;
+		lectred->last = l;
+	}
+	
+	return l->ma_cond;
+}
+
+void remove_thread_list(struct lectred* lectred) {
+	pthread_cond_destroy(lectred->first->ma_cond);
+	free(lectred->first->ma_cond);
+	if(lectred->first == lectred->last) { //1 seul élément dans la liste
+		free(lectred->first);
+		lectred->first = NULL;
+		lectred->last = NULL;
+	} else {
+		struct list * tmp = lectred->first;
+		lectred->first = lectred->first->suivant;
+		free(tmp);
 	}
 }
 
 void begin_read(struct lectred* lectred) {
 	pthread_mutex_lock(&lectred->mutex);
-	while(lectred->nb_w > 0) {
-		pthread_cond_wait( &lectred->lect, &lectred->mutex);
+	
+	pthread_cond_t * ma_cond = add_thread_list(lectred, 1);
+	
+	while(lectred->first != NULL && lectred->first->ma_cond != ma_cond || lectred->nb_w > 0) {
+		pthread_cond_wait( ma_cond, &lectred->mutex);
 	}
+	
 	lectred->nb_l ++;
+	remove_thread_list(lectred);
+	
+	if(lectred->first != NULL && lectred->first->est_lect) {
+		pthread_cond_signal(lectred->first->ma_cond);
+	}
+	
 	pthread_mutex_unlock(&lectred->mutex);
 }
 
@@ -51,32 +79,36 @@ void end_read(struct lectred* lectred) {
 	pthread_mutex_lock(&lectred->mutex);
 	lectred->nb_l --;
 	
-	if(lectred->nb_l == 0) {
-		pthread_cond_signal( &lectred->red);
+	if(lectred->nb_l == 0 && lectred->first != NULL && !lectred->first->est_lect) {
+		pthread_cond_signal( lectred->first->ma_cond);
 	}
+	
 	pthread_mutex_unlock(&lectred->mutex);
 }
 
 void begin_write(struct lectred* lectred) {
 	pthread_mutex_lock(&lectred->mutex);
-	while(lectred->nb_l > 0 || lectred->nb_w > 0) {
-		pthread_cond_wait( &lectred->red, &lectred->mutex);
+	
+	pthread_cond_t * ma_cond = add_thread_list(lectred, 0);
+	
+	while(lectred->first != NULL && lectred->first->ma_cond != ma_cond || lectred->nb_l > 0 || lectred->nb_w > 0) {
+		pthread_cond_wait( ma_cond, &lectred->mutex);
 	}
 	lectred->nb_w ++;
+	remove_thread_list(lectred);
 	pthread_mutex_unlock(&lectred->mutex);
 }
 
 void end_write(struct lectred* lectred) {
 	pthread_mutex_lock(&lectred->mutex);
 	lectred->nb_w --;
-	pthread_cond_broadcast( &lectred->lect);
-	pthread_cond_signal( &lectred->red);
+	if(lectred->first != NULL) {
+		pthread_cond_signal(lectred->first->ma_cond);
+	}
 	pthread_mutex_unlock(&lectred->mutex);
 }
 
 void lectred_destroy(struct lectred* lectred) {
 	pthread_mutex_destroy(&lectred->mutex);
-	pthread_cond_destroy( &lectred->red);
-	pthread_cond_destroy( &lectred->lect);
 	free(lectred);
 }
